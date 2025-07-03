4f65e2c858fb9745df9e9fa58fbc57f534208079
Letícia Cristina França Moraes Dos Santos <lleehmoraes@gmail.com>
2025-07-03 19:54:22 -0300

    updating code

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/shoppingreact",
+  base: "https://github.com/moraeslc17/e-plantShopping",
   plugins: [react()],
 })
